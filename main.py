ed3fa73743a46202f67ba9beb3b424367ffa7b9a
Juan Burtet <juan.burtet@gmail.com>
2018-08-22 11:30:00 -0300

    main comentado

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from datasetGenerator import DatasetLoader
 import time
 import csv
 
+# Lista de Atributos + Lista de Serviços
 attributeList = []
 serviceList = []
 
+# Adiciona os Atributos
 attributeList.append(Attribute("Response Time", maximized=False, weight=1, minValue=100, maxValue=400))
 attributeList.append(Attribute("Availability", maximized=True, weight=1, minValue=1, maxValue=100))
 attributeList.append(Attribute("Throughput", maximized=True, weight=1, minValue=1, maxValue=30))
@@ -17,59 +19,29 @@ attributeList.append(Attribute("Latency", maximized=False, weight=1, minValue=1,
 attributeList.append(Attribute("Successability", maximized=True, weight=1, minValue=1, maxValue=100))
 attributeList.append(Attribute("Agility", maximized=True, weight=1, minValue=1, maxValue=100))
 
+# Manda os atributos para o criador de dataset
 dataset = DatasetLoader(attributeList)
 
+# Pega o tempo do sistema
 startDataset = time.clock()
 
+# Gera um dataset de tamanho 500.000
 serviceList = dataset.generateRandom(500000)
 
-print("Tempo de geracao de dataset: " +
-      str(time.clock() - startDataset) + "s\n")
+# Imprime o tempo levado para gerar o Dataset
+print("Tempo de geracao de dataset: " + str(time.clock() - startDataset) + "s\n")
 
+# Pega o tempo do sistema
 start = time.clock()
 
+# Faz o calculo MCDA do Dataset gerado
 mcda = Mcda(serviceList, attributeList)
-
 mcda.normalizeData()
 mcda.calculateQuality()
 
+# Imprime o tempo levado para o calculo MCDA do dataset
 print("Tempo de classificacao: "+str(time.clock() - start)+"s\n")
 
+# Guarda os resultados em um arquivo csv
 mcda.storeResult()
 print("Os resultados foram salvos em: mcdaResult.csv\n\n")
-
-# mcda.printResult()
-
-# teste paralelo
-# with open("results.csv", "wb") as csv_file:
-#     writer = csv.writer(csv_file, delimiter=',')
-#     l = [500,1000,5000,50000,500000]
-#     for num in l:
-#         writer.writerow([num])
-#         mcda.loadFromRandom(num)
-#         #mcda.loadFromCSV('data.csv')
-
-#         # start = time.clock()
-
-#         # mcda.normalizeData()
-#         # mcda.calculateQuality()
-
-#         # print("sequencial time: "+str(time.clock() - start))
-#         for thr in range(1, 5):
-            
-#             writer.writerow([thr])            
-#             resultList = []
-#             for x in range(0, 9):
-#                 start = time.clock()
-
-#                 mcda.normalizeDataP(thr)
-#                 mcda.calculateQualityP(thr)
-
-#                 resultList.append(str(time.clock() - start).replace(".", ","))
-
-#             writer.writerow(resultList)
-
-
-# mcda.storeResult()
-
-# mcda.printResult()
